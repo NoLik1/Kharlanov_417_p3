9de5d5c1b6fb9992e4e13344beed7c6a160a587c
Evgenii <evgenuaaa@gmail.com>
2021-02-25 17:24:18 +0300

    bara

--- programmirovanie/laba 11.cpp
+++ programmirovanie/laba 11.cpp
@@ -7,50 +7,90 @@ using namespace std;
 struct Marvel {
     char Heroes [80];
     int age;
+    char city [80];
+};
+struct Superpowers{
+    int power;
     char superpowers [80];
+    char source_of_strength [80];
+};
+
+struct outcome {
+   Marvel Superheroes;
+   Superpowers Strengh;
 };
 
 void quantity (int &n) {
     cout<<"Введите количество требуемых вам героев\n"<<endl;
     cin>>n ;
 }
-void input (Marvel heroes [100],int &n){
+void input (outcome heroes [100],int &n){
     cout<<"Введите известных вам герове Марвел,заполнив три параметра - Имя,возраст, суперспособность"<<endl;
     for (int i = 0; i<n;  i++){
-        cout<<"Имя = "<<endl;
-        cin>>heroes[i].Heroes;
-        cout<<"Возраст = "<<endl;
-        cin>>heroes[i].age;
-        cout<<"Суперспособность = "<<endl;
-        cin>>heroes[i].superpowers;
+        cout<<"Имя = ";
+        cin>>heroes[i].Superheroes.Heroes;
+        cout<<"\nВозраст = ";
+        cin>>heroes[i].Superheroes.age;
+        cout<<"\nСуперспособность = ";
+        cin>>heroes[i].Strengh.superpowers;
+        cout<<"\n"<<endl;
     }
 }
 
-void Heroes (Marvel heroes [100], int &i){
+void Heroes (outcome heroes [100], int &i){
     cout<<"Введите номер вашего персонажа, а после введите его новое имя!\n"<<endl;
     cin >> i;
-    cin >> heroes[i].Heroes;
+    cin >> heroes[i].Superheroes.Heroes;
 }
 
-void age (Marvel heroes [100], int &i){
+void age (outcome heroes [100], int &i){
     cout<<"Введите номер вашего персонажа, а после введите его вверный возраст!\n"<<endl;
     cin >> i;
-    cin >> heroes[i].age;
+    cin >> heroes[i].Superheroes.age;
 }
 
-void superpowers (Marvel heroes [100], int &i){
+void superpowers (outcome heroes [100], int &i){
     cout<<"Введите номер вашего персонажа, а после введите его новую суперспособность!\n"<<endl;
     cin >> i;
-    cin >> heroes[i].superpowers;
+    cin >> heroes[i].Strengh.superpowers;
 }
 
-void output (Marvel  heroes [100],int &n){
+
+void output (outcome  heroes [100],int &n){
 
     for(int i = 0; i<n;i++){
-        cout<<"Имя = "<<heroes[i].Heroes<<"\n"<<endl;
-        cout<<"Возраст = "<<heroes[i].age<<"\n"<<endl;
-        cout<<"Суперспобосность = "<<heroes[i].superpowers<<"\n"<<endl;
+        cout<<"Имя = "<<heroes[i].Superheroes.Heroes<<"\n"<<endl;
+        cout<<"Возраст = "<<heroes[i].Superheroes.age<<"\n"<<endl;
+        cout<<"Суперспобосность = "<<heroes[i].Strengh.superpowers<<"\n"<<endl;
     }
+}
+
+
+void change (outcome heroes [100], int &i, int &r, bool choise = true) {
+    do{
+    cout<<"Вы выбрали параметр изменения вашего персонажа, 1 - изменение имени, 2 - возраста, 3 - суперспособности, 4 и далее - выход в основное меню."<<endl;
+    cin>>r;
+    switch (r){
+    case 1:
+        cout << "Вы выбрали параметр изменения имени вашего героя" <<endl;
+        Heroes(heroes,i);
+    break;
+    case 2:
+        cout << "Вы выбрали параметр изменения возраста вашего героя" <<endl;
+        age(heroes,i);
+    break;
+    case 3:
+        cout << "Вы выбрали параметр изменения суперспособности вашего героя" <<endl;
+        superpowers(heroes,i);
+    break;
+    case 4:
+        choise = false;
+        break;
+    default:
+        choise = false;
+    break;
+            }
+    }while (choise);
 }
 
 int main()
@@ -58,64 +98,31 @@ int main()
 setlocale(LC_ALL, "Russian");
    
     bool run = true;
-    bool choise = true;
-    Marvel heroes [100];
+    outcome heroes [100];
     int n,i,r;
     int abrakadabra;
     
-    cout<<"\t\t\tВведите номер вызываемой вами функции (1-ввод имён героев,второе-вызов справки, 3-е вызов плюшки)"<<endl;
-    cout<<"\t\t\tТакже, если вы ошиблись,нажмите 4 чтобы поменять какие-либо данные о нём, чтобы выйти из программы 5 или любую другую цифру! "<<endl;
+    
     
     while (run) {
+        cout<<"\t\t\tВведите номер вызываемой вами функции (1-ввод имён героев,второе-вызов справки, 3-е вызов плюшки)"<<endl;
+        cout<<"\t\t\tТакже, если вы ошиблись,нажмите 4 чтобы поменять какие-либо данные о нём, чтобы выйти из программы 5 или любую другую цифру! "<<endl;
     cin>>abrakadabra;
     switch (abrakadabra)  {
     case  1:
         quantity(n);
         input(heroes,n);
-        cout<<"\t\t\tВведите номер вызываемой вами функции (1-ввод имён героев,второе-вызов справки, 3-е вызов плюшки)"<<endl;
-        cout<<"\t\t\tТакже, если вы ошиблись,нажмите 4 чтобы поменять какие-либо данные о нём, чтобы выйти из программы 5 или любую другую цифру! "<<endl;
     break;
     case 2:
         output(heroes,n);
-        cout<<"\t\t\tВведите номер вызываемой вами функции (1-ввод имён героев,второе-вызов справки, 3-е вызов плюшки)"<<endl;
-        cout<<"\t\t\tТакже, если вы ошиблись,нажмите 4 чтобы поменять какие-либо данные о нём, чтобы выйти из программы 5 или любую другую цифру! "<<endl;
     break;
     case 3:
          cout << "Все супергероия - крассавчики" <<endl;
     break;
     case 4:
-        cout<<"Вы выбрали параметр изменения вашего персонажа, 1 - изменение имени, 2 - возраста, 3 - суперспособности, 4 и далее - выход в основное меню."<<endl;
-    while (choise){
-    cin>>r;
-    switch (r){
-    case 1:
-        cout << "Вы выбрали параметр изменения имени вашего героя" <<endl;
-        Heroes(heroes,i);
-        cout<<"Вы выбрали параметр изменения вашего персонажа, 1 - изменение имени, 2 - возраста, 3 - суперспособности, 4 и далее - выход в основное меню."<<endl;
+        change(heroes, i, r);
     break;
-    case 2:
-        cout << "Вы выбрали параметр изменения возраста вашего героя" <<endl;
-        age(heroes,i);
-        cout<<"Вы выбрали параметр изменения вашего персонажа, 1 - изменение имени, 2 - возраста, 3 - суперспособности, 4 и далее - выход в основное меню."<<endl;
-    break;
-    case 3:
-        cout << "Вы выбрали параметр изменения суперспособности вашего героя" <<endl;
-        superpowers(heroes,i);
-        cout<<"Вы выбрали параметр изменения вашего персонажа, 1 - изменение имени, 2 - возраста, 3 - суперспособности, 4 и далее - выход в основное меню."<<endl;
-    break;
-    case 4:
-        choise = false;
-        cout<<"\t\t\tВведите номер вызываемой вами функции (1-ввод имён героев,второе-вызов справки, 3-е вызов плюшки)"<<endl;
-        cout<<"\t\t\tТакже, если вы ошиблись,нажмите 4 чтобы поменять какие-либо данные о нём, чтобы выйти из программы 5 или любую другую цифру! "<<endl;
-    break;
-    default:
-        choise = false;
-        cout<<"\t\t\tВведите номер вызываемой вами функции (1-ввод имён героев,второе-вызов справки, 3-е вызов плюшки)"<<endl;
-        cout<<"\t\t\tТакже, если вы ошиблись,нажмите 4 чтобы поменять какие-либо данные о нём, чтобы выйти из программы 5 или любую другую цифру! "<<endl;
-    break;
-            }
-    }
-    case 10:
+    case 5:
          run = false;
          cout<<"\t\t\tПриходите к нам ещё"<<endl;
     break;
